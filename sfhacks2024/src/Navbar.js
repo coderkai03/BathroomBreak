d96e12bcd8f71f83e678cfc9e38e3cd1b59b6cd4
Riankai1073 <riancorci@gmail.com>
2024-04-05 20:19:39 -0700

    created navbar

---
+++ sfhacks2024/src/Navbar.js
@@ -0,0 +1,17 @@
+import {Link} from 'react-router-dom'
+
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <h1>Bathroom Break</h1>
+            <div className="links">
+                <Link to='/'>Play</Link>
+                <Link to='/map' >Map</Link>
+                <Link to='/feedback' >Feedback</Link>
+                {/* onClick={ (event) => event.preventDefault() }  */}
+            </div>
+        </nav>
+    );
+}
+ 
+export default Navbar;
