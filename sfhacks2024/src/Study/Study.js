93bfabf6fad335570e594d3d73c050a4fa623486
Hanson Vuong <hansonvuong@berkeley.edu>
2024-04-06 17:36:50 -0700

    Study page banner, Flashcard functionality needed

--- sfhacks2024/src/Study/Study.js
+++ sfhacks2024/src/Study/Study.js
@@ -1,10 +1,13 @@
 import { useState } from "react"
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import './Study.css';
 
 const Study = () => {
     
     return (
-        <div>
-
+        <div className="top-banner">
+                <h1> Flashcards</h1>
+                <h2>Click on a study set to begin.</h2>
         </div>
     )
 }
